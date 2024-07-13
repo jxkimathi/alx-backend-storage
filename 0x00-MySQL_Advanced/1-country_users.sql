00a4de2d0034c8bb46ad12dbfb1e5b8534060cad
jxkimathi <judescriva8@gmail.com>
2024-07-13 21:37:03 +0300

    minor change to the attributes

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Write a SQL script that creates a table users following these requirements:
 --id, email, name, country
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
