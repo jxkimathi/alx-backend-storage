88f7e201bd8988cc18d8543b120861bf7721d3dc
jxkimathi <judescriva8@gmail.com>
2024-08-29 12:08:16 +0300

    advanced task of the redis basic project

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,50 @@
 import redis
 from typing import Union, Optional, Callable, Any
 import uuid
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """Wrapper function that counts number of times methods called"""
+    key: str = method.__qualname__
+
+    @wraps(method)
+    def increment_calls(self, *args: Any, **kwargs: Any) -> Any:
+        """Increment the calls method"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return increment_calls
+
+def call_history(method: Callable) -> Callable:
+    """Stores the history of inputs and outputs for a method"""
+    @wraps(method)
+    def store_history(self, *args: Any, **kwargs: Any) -> Any:
+        """Stores the history of inputs and outputs for a method"""
+        input_key: str = str(method.__qualname__) + ":inputs"
+        self._redis.rpush(input_key, str(args))
+
+        output_key: str = str(method.__qualname__) + ":outputs"
+        output: Any = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+        return output
+    return store_history
+
+def replay(methos: Callable) -> None:
+    """Displays the history of calls of a method"""
+    r = redis.Redis()
+    input_key: str = str(method.__qualname__) + ":inputs"
+    output_key: str = str(method.__qualname__) + ":outputs"
+
+    number_of_calls: int = int(r.get(method.__qualname__))
+    print("{} was called {} times:".format(method.__qualname__,
+                                           str(number_of_calls)))
+    inputs: List = r.lrange(input_key, 0, -1)
+    outputs: List = r.lrange(output_key, 0, -1)
+    history: List = list(zip(inputs, outputs))
+    for pair in history:
+        print("{}(*{}) -> {}".format(method.__qualname__,
+                                     pair[0].decode("utf-8"),
+                                     pair[1].decode("utf-8")))
 
 class Cache:
     """Redis class that stores instance of the redis client"""
@@ -12,6 +55,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in the redis instance"""
         key: str = str(uuid.uuid4())
